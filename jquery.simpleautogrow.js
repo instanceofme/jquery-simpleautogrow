b85902307832eceee11242b679cba7899cae198e
streetpc <streetpc@free.fr>
2010-12-21 02:50:10 -0800

    Added margin argument (in pixels, TODO add possibility of css-style string)

--- jquery.simpleautogrow.js
+++ jquery.simpleautogrow.js
@@ -12,10 +12,10 @@
 
 (function(jQuery) {
 
-	jQuery.fn.simpleautogrow = function() {
-		return this.each(function() { new jQuery.simpleautogrow(this); }); };
+	jQuery.fn.simpleautogrow = function(margin) {
+		return this.each(function() { new jQuery.simpleautogrow(this, margin); }); };
 
-	jQuery.simpleautogrow = function (e) {
+	jQuery.simpleautogrow = function (e, margin) {
 		var self = this;
 		var $e = this.textarea = jQuery(e)
 			.css({overflow: 'hidden', display: 'block'})
@@ -23,13 +23,15 @@
 				this.timer = window.setInterval(function() {self.checkExpand(); }, 200); })
 			.bind('blur', function() { clearInterval(this.timer); });
 		this.border = $e.outerHeight() - $e.innerHeight();
+		try { margin = typeof margin == 'undefined' ? 0 : parseInt(margin); } catch(ex) { margin = 0; }
+		this.margin = margin;
 		this.clone = $e.clone().css({position: 'absolute', visibility: 'hidden'}).attr('name', '')
 		$e.height(e.scrollHeight + this.border)
 			.after(this.clone);
 		this.checkExpand(); };
 
 	jQuery.simpleautogrow.prototype.checkExpand = function() {
-		var target_height = this.clone[0].scrollHeight + this.border;
+		var target_height = this.clone[0].scrollHeight + this.border + this.margin;
 		if (this.textarea.outerHeight() != target_height)
 			this.textarea.height(target_height + 'px');
 		this.clone.attr('value', this.textarea.attr('value')).height(0); };
